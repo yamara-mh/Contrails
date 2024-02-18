c1fa7fed71b58fa5d2911cd68379351ee031298e
usounds <grave.evarg@gmail.com>
2024-02-18 17:03:18 +0900

    ソースのリファクタ

--- cloudflare-worker/bsky-search.js
+++ cloudflare-worker/bsky-search.js
@@ -5,17 +5,13 @@ export async function searchPost(searchTerm, params,accessJwt) {
     q: searchTerm,
   };
   if (params.count !== undefined) {
-    // @ts-expect-error TS(2339): Property 'limit' does not exist on type '{ q: any;... Remove this comment to see the full error message
     urlParams.limit = params.count;
   }
   if (params.offset !== undefined) {
-    // @ts-expect-error TS(2339): Property 'cursor' does not exist on type '{ q: any... Remove this comment to see the full error message
     urlParams.cursor = params.offset;
   }
   let url =
     "https://bsky.social/xrpc/app.bsky.feed.searchPosts?" + new URLSearchParams(urlParams);
-    console.log(accessJwt);
-  //let response = await fetchGuardedWithLogin(url,accessJwt);
 
   let response = await fetchGuarded(url, {
     headers: {
