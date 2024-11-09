6734f6048e9c4f11b26f31fb84a7a512e34daf7b
yamara <39893033+yamara-mh@users.noreply.github.com>
2024-11-10 02:31:51 +0900

    Update bsky-feedgen-who-liked-me.js

--- cloudflare-worker/bsky-feedgen-who-liked-me.js
+++ cloudflare-worker/bsky-feedgen-who-liked-me.js
@@ -303,11 +303,25 @@ export async function getFeedSkeleton(request, env) {
   }
 
 
+
+
+
+
+
+
+  
   // const did = new DidResolver({}).did;
   // if (did == undefined) throw new Error('expected handle to resolve');
   // else console.log(did);
-  const did = new IdResolver().did;
-  console.log(did);
+  // const did = new IdResolver().did;
+  // console.log(did);
+
+
+
+
+
+  
+
   
   
 
