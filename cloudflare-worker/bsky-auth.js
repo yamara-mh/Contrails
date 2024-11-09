452e93ad93c5b9dcc0039e104ca410d736d4088c
yamara <39893033+yamara-mh@users.noreply.github.com>
2024-11-10 04:08:06 +0900

    Update bsky-auth.js

--- cloudflare-worker/bsky-auth.js
+++ cloudflare-worker/bsky-auth.js
@@ -13,13 +13,6 @@ async function login(username, password, env) {
 
   const savedJwt = results[0].accessJwt;
   const sessionCheck = 'https://bsky.social/xrpc/com.atproto.server.getSession';
-
-  console.log("bsky-auth username");
-  console.log(...username);
-  console.log("bsky-auth env");
-  console.log(env);
-  console.log("bsky-auth savedJwt");
-  console.log(...savedJwt);
   let checkResult = await fetch(sessionCheck,{
     headers: {
       Authorization: `Bearer ${savedJwt}`,
