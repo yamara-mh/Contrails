0a62281cb8fb6dcc2566f63bb9373c054cc6c85c
yamara <39893033+yamara-mh@users.noreply.github.com>
2024-11-10 19:14:40 +0900

    Update bsky-feedgen-who-liked-me.js

--- cloudflare-worker/bsky-feedgen-who-liked-me.js
+++ cloudflare-worker/bsky-feedgen-who-liked-me.js
@@ -260,7 +260,9 @@ export async function getFeedSkeleton(request, env) {
   console.log("jwt");
   const jwt = request.headers.get("Authorization");
   console.log(jwt);
-  var userDid = JSON.parse(atob(jwt.toString().split(" ")[1]));
+  const a = jwt.toString().split(" ")[1].split(".")[1];
+  console.log(a);
+  var userDid = JSON.parse(atob(a));
   console.log(userDid);
   console.log(userDid.toString());
   console.log(userDid.iss);
