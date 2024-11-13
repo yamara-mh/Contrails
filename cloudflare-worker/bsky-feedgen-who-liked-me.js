62b36528e460824284ba940f60feffd84bb4c179
yamara <39893033+yamara-mh@users.noreply.github.com>
2024-11-14 02:03:48 +0900

    update

--- cloudflare-worker/bsky-feedgen-who-liked-me.js
+++ cloudflare-worker/bsky-feedgen-who-liked-me.js
@@ -358,8 +358,8 @@ export async function getFeedSkeleton(request, env) {
   const likedUserResults = await Promise.allSettled(
     filteredPosts.map(async item => {
       console.log(Object.values(item));
-      console.log(item.uri);
-      return await fetchLikes(accessJwt, item.uri, GET_LIKES_USER);
+      console.log(item.uri.toString());
+      return await fetchLikes(accessJwt, item.uri.toString(), GET_LIKES_USER);
     }));
 
   console.log(Object.values(likedUserResults));
